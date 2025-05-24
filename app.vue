aadf67836426ba3767b3e587285ea7adca109483
EwdErna <ewderna@gmail.com>
2025-05-24 23:00:42 +0900

    initial commit

---
+++ app.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="app-container">
+    <nav>Navigation</nav>
+    <main>
+      <section class="left">
+      </section>
+      <section class="main">
+      </section>
+      <section class="right">
+      </section>
+    </main>
+    <footer></footer>
+  </div>
+</template>
+
+<style scoped>
+.app-container {
+  height: calc(100vh - 16px);
+
+  nav {
+    height: 64px;
+  }
+
+  footer {
+    height: 20px;
+  }
+
+  main {
+    width: 100%;
+    height: calc(100% - 84px);
+    display: flex;
+
+    .left {
+      width: 25%;
+      background-color: #f0f0f0;
+    }
+
+    .right {
+      width: 25%;
+      background-color: #f0f0f0;
+    }
+
+    .main {
+      width: 50%;
+    }
+  }
+}
+</style>
